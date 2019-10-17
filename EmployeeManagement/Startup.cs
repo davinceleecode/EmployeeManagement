aa10c7b3c65be8d9f49bb4bcfe5c0e84a09e6b69
Vincent Lee Flores <wiz22vincent@gmail.com>
2019-10-17 22:31:47 +0800

    adding new sourcecode

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                #region  comment:   DeveloperExceptionPageOptions Customize
+                //DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
+                //{
+                //    SourceCodeLineCount = 10
+                //};
+                #endregion
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            #region comment: UseDefaultFiles Middleware
+            //custom default file middleware
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseDefaultFiles(defaultFilesOptions);
+            #endregion
+
+            #region comment: UseFileServer Middleware
+            //custom default fileServer
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseFileServer(fileServerOptions);
+            #endregion
+
+            #region comment: UseStaticFiles Middleware
+            //remove by using the usefileserver middleware
+            //app.UseDefaultFiles(defaultFilesOptions);
+            //app.UseStaticFiles();
+            #endregion
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hosting Environment:{ env.EnvironmentName }");
+            });
+        }
+    }
+}
