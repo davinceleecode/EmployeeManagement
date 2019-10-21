2cd547da8d53f8b96fd4f36a23b0cb81ec78803d
Vincent Lee Flores <wiz22vincent@gmail.com>
2019-10-21 17:08:09 +0800

    adding views

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -15,7 +15,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddXmlSerializerFormatters();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
@@ -33,7 +33,7 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-
+             
             #region comment: UseDefaultFiles Middleware
             //custom default file middleware
             //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
