1e805cd5673e07274cb285212065f8fbd56aa4f3
Vincent Lee Flores <wiz22vincent@gmail.com>
2019-10-28 23:32:40 +0800

    Form tag helpers

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -12,9 +12,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() {Id = 1, Name = "Mary", Department = "HR", Email = "mary@davinceleecode.com"},
-                new Employee() {Id = 2, Name = "Rose", Department = "IT", Email = "rose@davinceleecode.com"},
-                new Employee() {Id = 3, Name = "Lee", Department = "IT", Email = "lee@davinceleecode.com"}
+                new Employee() {Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@davinceleecode.com"},
+                new Employee() {Id = 2, Name = "Rose", Department = Dept.IT, Email = "rose@davinceleecode.com"},
+                new Employee() {Id = 3, Name = "Lee", Department = Dept.IT, Email = "lee@davinceleecode.com"}
             };
         }
         public Employee GetEmployee(int Id)
