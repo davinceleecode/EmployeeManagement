ed9e1ad14b75e03d2dc5d645b153eda328956532
Vincent Lee Flores <wiz22vincent@gmail.com>
2019-11-05 22:48:22 +0800

    Repository pattern in asp net core

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -27,7 +27,7 @@ namespace EmployeeManagement
                 options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
